32157c22315468de165db7e58fc6620a98843f5e
TejuGabbu <tskulkarni7875900168@gmail.com>
2023-09-13 13:19:28 +0530

    changes done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
-public class Main {
-  
+public class Main
+{
+    public static void main(String[] args)
+    {
+        RWOnly rwOnly = new RWOnly();
+        rwOnly.setName("tejas");
+        String result = rwOnly.getName();
+        System.out.println(result);
+    }
 }
